52afb17a87b60058476d1dea54bc32196f450890
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 15:30:15 +0300

    проверка nullable

---
+++ Module_6/Exercise_6_0_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise_6_0_1
+{
+    internal class Program
+    {
+        static void Show(int? obj)
+        {
+            Console.WriteLine($"{obj ?? null}\n");
+        }
+        static void Main(string[] args)
+        {
+            int? a = null;
+            Show(a);
+            a = 10;
+            Show(a);
+            
+        }
+    }
+}
