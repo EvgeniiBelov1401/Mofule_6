27bdc2984d68fdc4f8e1a939a63162d25bf026f7
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 16:41:53 +0300

    Добавил Exercise_6_6_1

---
+++ Module_6/Exercise_6_6_1/TrafficLight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6_6_1
+{
+    internal class TrafficLight
+    {
+        private void ChangeColor(string color)
+        {
+
+        }
+        public string GetColor()
+        {
+            
+        }
+    }
+}
