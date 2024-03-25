b1f85545b6a5b3b34245678a6926fe2e462430b9
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 11:47:16 +0300

    Добавил Exercise_6_2_0_1

---
+++ Module_6/Exercise_6_2_0_1/Human.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6_2_0_1
+{
+    internal class Human
+    {
+        public string name;
+        public int age;
+
+        public void Greeting()
+        {
+            Console.WriteLine($"Меня зовут {name}, мой возраст: {age}\n");
+        }
+    }
+}
---
+++ Module_6/Exercise_6_2_0_1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise_6_2_0_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+            human.Greeting();
+            human.name = "Евгений";
+            human.age = 34;
+            human.Greeting();
+
+        }
+    }
+}
