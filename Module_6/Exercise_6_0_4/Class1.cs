fc333ab90d86fb414c7fc071278012d3903c4812
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 18:47:31 +0300

    check

---
+++ Module_6/Exercise_6_0_4/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6_0_4
+{
+    internal class Class
+    {
+        public int A 
+        {
+            get
+            {
+                return 5;
+            }
+           
+        }
+        public int B 
+        { private get { return B; }
+          set { B = value; } 
+        }
+
+        public int C {private get; set; }
+    }
+}
---
+++ Module_6/Exercise_6_0_4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise_6_0_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class class1 = new Class();
+            Console.WriteLine(class1.A);
+            
+        }
+    }
+}
