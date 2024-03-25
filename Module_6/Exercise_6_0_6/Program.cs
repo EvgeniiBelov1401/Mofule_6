42c4d23e74e7e9f68ac9538de9baa51a2893faf1
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 21:11:43 +0300

    check

---
+++ Module_6/Exercise_6_0_6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise_6_0_6
+{
+    internal class Program
+    {
+        static void AddOne(out int? result)
+        {
+            result = 0;
+            result++;
+        }
+        static void Main(string[] args)
+        {
+            int? a=null;
+            Console.WriteLine(a);
+            AddOne(out a);
+            Console.WriteLine(a);
+        }
+    }
+}
