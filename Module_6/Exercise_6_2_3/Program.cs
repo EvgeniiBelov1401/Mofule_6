56aba787b90ccc77422b3bf1e9f47a727387beb7
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 12:50:37 +0300

    Добавил Exercise_6_2_3

---
+++ Module_6/Exercise_6_2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise_6_2_3
+{
+    internal class Program
+    {
+        public struct Animal
+        {
+            public string type;
+            public string name;
+            public int age;
+        }
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal() {type="Собака", name="Вольт", age=4};
+        }
+    }
+}
