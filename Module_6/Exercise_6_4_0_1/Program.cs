f3675cff04a971841a1419348847af46d2796eeb
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 15:54:43 +0300

    check

---
+++ Module_6/Exercise_6_4_0_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_6_4_0_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            Console.WriteLine(a);
+
+            object o = a; 
+            Console.WriteLine(o);
+
+            object o1=o;
+            o1 = 321;
+            Console.WriteLine(o1+" "+o);
+
+            int b = (int)o;
+            Console.WriteLine(b);
+
+
+        }
+    }
+}
