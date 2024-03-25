13be769542f1fce3cf28ace0e1d1755a9830fd94
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 12:11:16 +0300

    Добавил Exercise_6_2_2

---
+++ Module_6/Exercise_6_2_2/Pen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6_2_2
+{
+    internal class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100;
+        }
+        public Pen(string penColor,int penCost)
+        {
+            color= penColor;
+            cost = penCost;
+        }
+    }
+}
---
+++ Module_6/Exercise_6_2_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_6_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
