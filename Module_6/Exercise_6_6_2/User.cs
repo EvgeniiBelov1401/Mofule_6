a6371ccb598f4b3913a6eea4d909ea02cec9e0ea
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 16:57:24 +0300

    Добавил Exercise_6_6_2

---
+++ Module_6/Exercise_6_6_2/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6_6_2
+{
+    internal class User
+    {
+        private string _login;
+        private string _mail;
+
+        public string Login
+        {
+            get { return _login; }
+            set 
+            { 
+                if (_login.Length < 3)
+                { 
+                        Console.WriteLine("Логин не менее 3х символов");
+                }
+                else
+                {
+                    _login = value;
+                }
+                
+            }
+        }
+
+        public string Mail
+        {
+            get { return _mail; }
+            set 
+            {
+                if (value.Contains('@'))
+                {
+                    _mail = value;
+                }
+                else
+                {
+                    Console.WriteLine("Почта должна содержать @");
+                }
+            }
+        }
+    }
+}
