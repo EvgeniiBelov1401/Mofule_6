c5640eeced4b13bf0ac87fab3d4d975e6144b9d1
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 14:51:13 +0300

    Добавил Exercise_6_3_2

---
+++ Module_6/Exercise_6_3_2/Bus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6_3_2
+{
+    internal class Bus
+    {
+        public int? Load;
+        public bool? HasLoad;
+
+        public void PrintStatus()
+        {
+            if (HasLoad==true && Load>0)
+            {
+                Console.WriteLine($"В автобусе {Load} пассажиров");
+            }
+            else
+            {
+                Console.WriteLine($"Автобус пустой");
+            }
+        }
+        public Bus(Load load)
+        {
+            Load=load.Value;
+            HasLoad=load.HasValue;
+        }
+
+
+    }
+}
---
+++ Module_6/Exercise_6_3_2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_6_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus bus = new Bus(new Load(false ,10));
+            bus.PrintStatus();
+        }
+    }
+}
