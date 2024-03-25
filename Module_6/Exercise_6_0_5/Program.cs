60a0bb9e5695945bec41f9db1257b59aa5c63141
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 20:05:29 +0300

    check

---
+++ Module_6/Exercise_6_0_5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise_6_0_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Фраза: ");
+            string str = Console.ReadLine();
+            //Console.Write("Фраза наоборот: ");
+            //for (int i = str.Length-1; i >= 0; i--)
+            //{
+            //    Console.Write(str[i]);
+            //}
+
+            string str1 = str;
+            str1 = "no";
+        }
+    }
+}
