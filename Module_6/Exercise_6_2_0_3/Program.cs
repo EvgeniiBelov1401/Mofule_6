0aca9a1d4bc814f0123534b393e12fc716a46256
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 12:14:53 +0300

    Добавил Exercise_6_2_0_3

---
+++ Module_6/Exercise_6_2_0_3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_6_2_0_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human() {name="Евгений", age=34};
+            human.Greeting();
+        }
+    }
+}
