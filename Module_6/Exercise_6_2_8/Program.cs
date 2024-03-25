af34020906f2bae7d7b768314f42cf8f2a97a556
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 13:00:19 +0300

    Добавил Exercise_6_2_8

---
+++ Module_6/Exercise_6_2_8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise_6_2_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine(rectangle.Square()+"\n");
+
+            rectangle= new Rectangle(2,3);
+            Console.WriteLine(rectangle.Square() + "\n");
+
+            rectangle = new Rectangle(10);
+            Console.WriteLine(rectangle.Square() + "\n");
+        }
+    }
+}
---
+++ Module_6/Exercise_6_2_8/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6_2_8
+{
+    internal class Rectangle
+    {
+        public int a;
+        public int b;
+
+        public int Square()
+        {
+            return a * b;
+        }
+
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
+        }
+
+        public Rectangle(int x,int y)
+        {
+            a=x; 
+            b=y;
+        }
+
+        public Rectangle(int x)
+        {
+            a = x;
+            b = x;
+        }
+    }
+
+    
+}
