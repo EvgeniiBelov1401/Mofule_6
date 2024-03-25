bd96b1cfa42e51eaa123a7de4e32dc58b928f8a8
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 11:14:58 +0300

    Добавил Exercise_6_0_2

---
+++ Module_6/Exercise_6_0_2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise_6_0_2
+{
+    internal class Program
+    {
+        static void AddTen(ref int num)
+        {
+            num = num + 10;
+        }
+        static void Main(string[] args)
+        {
+            var num = 1;
+            AddTen(ref num);
+
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+    }
+}
