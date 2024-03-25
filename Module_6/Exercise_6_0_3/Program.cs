d7a767afdbc2eba01221c1e84fcd5b0861841ecb
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-25 18:32:49 +0300

    check

---
+++ Module_6/Exercise_6_0_3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise_6_0_3
+{
+    internal class Program
+    {
+        static void ShowArray(int[]arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();    
+        }
+        static void Main(string[] args)
+        {
+            int[] array0 = { 1, 2, 3, 4, 5 };
+            ShowArray(array0);
+
+            int[] array1 = array0;
+            ShowArray(array1);
+            array1[4] = 0;
+            ShowArray(array1);
+            ShowArray(array0);
+        }
+    }
+}
